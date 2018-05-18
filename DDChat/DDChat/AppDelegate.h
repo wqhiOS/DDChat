5de2ab87be0e9fcee5549e48fd96cc60f62eb67e
wuqihan <wuqihan@able-elec.com>
2018-05-18 10:14:31 +0800

    init

---
+++ DDChat/DDChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDChat
+//
+//  Created by wuqh on 2018/5/18.
+//  Copyright © 2018年 wuqh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DDChat/DDChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DDChat
+//
+//  Created by wuqh on 2018/5/18.
+//  Copyright © 2018年 wuqh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
